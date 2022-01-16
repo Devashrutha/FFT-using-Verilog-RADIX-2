ae64d91a03d760db72ef27ea0b61c2e68489af79
Devashrutha <61559101+Devashrutha@users.noreply.github.com>
2022-01-16 21:09:43 +0000

    Update fft_testbench.v

--- fft_testbench.v
+++ fft_testbench.v
@@ -1,36 +1,33 @@
-`timescale 1ns/1ns
-module testbench;
+`timescale 1ns/1ns 
+module testbench; 
 reg clk;
 reg [2:0] sel;
 wire [8:0] yr,yi;
-dit_fft_8 dit_fft_8_test(.clk(clk),.sel(sel),.yr(yr),.yi(yi));
-  
+dit_fft_8 dit_fft_8_test(.clk(clk),.sel(sel),.yr(yr),.yi(yi)); 
 initial
+begin clk = 0;
+end 
+always 
 begin
-clk = 1;
+//TOGGLE CLOCK EVERY 5ns
+#5
+clk = ~clk;
 end
-
-always
+initial 
 begin
-//TOGGLE THE CLOCK FOR EVERY 5ns
-#5 clk = ~clk;
-end
-
-initial
-begin
-sel = 3'b000;#10
-sel = 3'b001;#10
-sel = 3'b010;#10
-sel = 3'b011;#10
-sel = 3'b100;#10
-sel = 3'b101;#10
-sel = 3'b110;#10
-sel = 3'b111;#10
-$finish();
-end
+sel = 3'b000;#10 
+sel = 3'b001;#10 
+sel = 3'b010;#10 
+sel = 3'b011;#10 
+sel = 3'b100;#10 
+sel = 3'b101;#10 
+sel = 3'b110;#10 
+sel = 3'b111;
+end 
 initial begin
-  $dumpfile("dump.vcd");
-  $dumpvars;
-  #10000 $finish;
+$dumpfile("dump1.vcd");
+$dumpvars;
+#10000 $finish;
 end
+  
 endmodule
